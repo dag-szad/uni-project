7bcbc817b70b15e92fc1d4dc3e5f6c2091a43b81
Dag <dagmara.szadkowska@wp.pl>
2023-11-29 19:02:22 +0100

    Index | Add modal handler and login authentication

---
+++ index.js
@@ -0,0 +1,52 @@
+// Query selectors
+const logInButton = document.querySelector(".loginButton");
+const modal = document.querySelector(".overlay");
+const exitButton = document.querySelector(".modal-exit");
+
+// Toggle modal
+function toggleModal() {
+  modal.classList.toggle("is-hidden");
+}
+
+// Switch open/close
+function handleModalToggle(event) {
+  switch (event.type) {
+    case "click":
+      toggleModal();
+      break;
+    case "keydown":
+      if (event.key === "Escape") {
+        toggleModal();
+      }
+      break;
+    default:
+      break;
+  }
+}
+
+// Events
+logInButton.addEventListener("click", handleModalToggle);
+exitButton.addEventListener("click", handleModalToggle);
+window.addEventListener("keydown", handleModalToggle);
+
+// Query selectors
+const emailInput = document.querySelector(".emailInput");
+const passwordInput = document.querySelector(".passwordInput");
+const submitButton = document.querySelector(".modal-submit");
+
+// Login data
+const email = "user@mail.com";
+const password = "user1";
+
+// Login authentication
+function logIn() {
+  if (emailInput.value === email && passwordInput.value === password) {
+    window.location.href = "home.html";
+  } else if (emailInput.value !== email || passwordInput.value !== password) {
+    window.alert("Invalid user data");
+  } else {
+    console.log("Some error");
+  }
+}
+
+submitButton.addEventListener("click", logIn);
